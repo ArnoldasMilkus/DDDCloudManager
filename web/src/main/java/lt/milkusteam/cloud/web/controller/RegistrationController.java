a97885d96ef90d10f3e1a48f5529ab767f68eb01
gedkur <gvkperson@gmail.com>
2016-05-15 17:49:24 +0300

    Fixed imports

--- web/src/main/java/lt/milkusteam/cloud/web/controller/RegistrationController.java
+++ web/src/main/java/lt/milkusteam/cloud/web/controller/RegistrationController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.ejb.EJB;
 import javax.validation.Valid;
 import java.util.Calendar;
 import java.util.Locale;
